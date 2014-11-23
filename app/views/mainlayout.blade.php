984ffe41dde11bb0f97821c6018a1be83a162421
seanachaidh <pvankeymeulen@seanachaidh.be>
2014-11-23 16:05:40 +0100

    Een eerste commit

---
+++ app/views/mainlayout.blade.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<!-- Voor dit project gaan we bootstrap gebruiken -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/boostrap-theme.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/3.3.0/js/boostrap.min.js"></script>
+	</head>
+	<body>
+		<!-- ik moet ervoor zorgen dat dit vertaald kan worden -->
+		<h1 id="mainheading">Welkom bij seanachaidh</h1>
+		<div class="content">
+			@yield('content')
+		</div>
+	</body>
+</html>
